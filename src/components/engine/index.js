f4a53ca15719c701757b7eebd6ff297df0d81098
Thomas Stussi <tstussi06@gmail.com>
2020-09-18 13:50:51 -0700

    add event listener hook

--- src/components/engine/index.js
+++ src/components/engine/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import styles from './engine.module.scss';
+import { useDidMount, useEvent } from '../../hooks';
+
 
 export default function Engine() {
+    const handleKeyPress = (e) => {
+        if (e.key === ' ') {
+            console.log('SPACEBARRRRRRR')
+        }
+    };
+
+    useEvent('keyup', handleKeyPress);
+
     return (
         <div
           className={styles.container}
